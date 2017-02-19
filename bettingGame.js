b14d7e7197f117ae012c7207502ac149fe1a65dc
ScotiaGaming9090 <JasonLumsden9090@outlook.com>
2017-02-18 19:29:14 -0500

    Update bettingGame.js

--- bettingGame.js
+++ bettingGame.js
@@ -14,10 +14,29 @@
 	// spritesheet object
 	var spriteSheet = null;
 	// game objects
-
-
-
-
+    var trafficLight = null; // Used in the race screen to countdown the animation
+    var trafficLightMover = null;
+    // Wheel # One
+    var wheel_01 = null; // Racing Object #1
+    var wheel_01Mover = null;
+    // Wheel # Two
+    var wheel_02 = null; // Racing Object #2
+    var wheel_02Mover = null;
+    // Wheel # Three
+    var wheel_03 = null; // Racing Object #3
+    var wheel_03Mover = null;
+    // Wheel # Four
+    var wheel_04 = null; // Racing Object #4
+    var wheel_04Mover = null;
+    
+    
+    // background screens ?? - might not need
+    var betScreen = null;
+    var summaryScreen = null;
+    var raceScreen = null;
+    var chooseScreen = null;
+    
+    
 	// spritesheet data object
 	var data = {
 	"framerate":24,
@@ -148,6 +167,65 @@
 	function onReady(e) {
 		console.log(">> adding sprites to game");
 		spriteSheet.removeEventListener("complete", onReady);
+        
+        // construct the traffic light sprite
+	    trafficLight = new createjs.Sprite(spriteSheet);
+		trafficLight.x = 250;
+		trafficLight.y = 300;
+		trafficLight.gotoAndStop("trafficLight");
+		stage.addChild(trafficLight);
+		trafficLightMover = new Mover(trafficLight, stage);
+		trafficLightMover.setSpeed(6);
+		trafficLightMover.startMe();
+		trafficLightMover.setDirection(2)
+        
+ 
+        // construct the wheel sprite
+        // For Wheel # One
+	    wheel_01 = new createjs.Sprite(spriteSheet);
+		wheel_01.x = 50;
+		wheel_01.y = 50;
+		wheel_01.gotoAndStop("wheel");
+		stage.addChild(wheel_01);
+		wheel_01Mover = new Mover(wheel_01, stage);
+		wheel_01Mover.setSpeed(6);
+		wheel_01Mover.startMe();
+		wheel_01Mover.setDirection(2)
+        
+        // For Wheel # Two
+        wheel_02 = new createjs.Sprite(spriteSheet);
+		wheel_02.x = 50;
+		wheel_02.y = 120;
+		wheel_02.gotoAndStop("wheel");
+		stage.addChild(wheel_02);
+		wheel_02Mover = new Mover(wheel_02, stage);
+		wheel_02Mover.setSpeed(6);
+		wheel_02Mover.startMe();
+		wheel_02Mover.setDirection(2)
+        
+                
+        // For Wheel # Three
+        wheel_03 = new createjs.Sprite(spriteSheet);
+		wheel_03.x = 50;
+		wheel_03.y = 190;
+		wheel_03.gotoAndStop("wheel");
+		stage.addChild(wheel_03);
+		wheel_03Mover = new Mover(wheel_03, stage);
+		wheel_03Mover.setSpeed(6);
+		wheel_03Mover.startMe();
+		wheel_03Mover.setDirection(2)
+        
+                        
+        // For Wheel # Four
+        wheel_04 = new createjs.Sprite(spriteSheet);
+		wheel_04.x = 50;
+		wheel_04.y = 260;
+		wheel_04.gotoAndStop("wheel");
+		stage.addChild(wheel_04);
+		wheel_04Mover = new Mover(wheel_04, stage);
+		wheel_04Mover.setSpeed(6);
+		wheel_04Mover.startMe();
+		wheel_04Mover.setDirection(2)
 
         
 		console.log(">> game ready");
@@ -158,9 +236,15 @@
 		// TESTING FPS
 		document.getElementById("fps").innerHTML = createjs.Ticker.getMeasuredFPS();
 
-		// game loop code here
-		
+        // game loop code here
+//	      wheel_01Mover.updateMe();
+//        wheel_02Mover.updateMe();
+//        wheel_03Mover.updateMe();
+//        wheel_04Mover.updateMe();
+//        trafficLightMover.updateMe();
+        
 		// update the stage!
+		stage.update();
 	}
 
 })();
